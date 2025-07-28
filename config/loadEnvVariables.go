e05128b1ca2c4cf02a253de50cdb2c4819eae59c
Adam Salinggih <ahazveroz@live.com>
2025-07-27 20:29:03 -0700

    New Files

---
+++ config/loadEnvVariables.go
@@ -0,0 +1,16 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnvVariables() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading godotevent:", err)
+		return
+	}
+	fmt.Println("godotevent loaded successfully")
+}
